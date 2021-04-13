899b61cab9826d23df74aee5f014b70baf5e66a4
Matthew Day <matt@matthewday.net>
2021-04-13 18:42:56 -0400

    refactor to improve readability

--- src/components-assignment/App.js
+++ src/components-assignment/App.js
@@ -1,72 +1,38 @@
 import React from 'react';
-import './App.css';
 import List from './List.js';
+import './App.css';
+import data from './mocks.json';
+
 
 class App extends React.Component {
 
-    state = {
-        storeIntermediate: {
-            lists: [
-              {
-                id: '1',
-                header: 'First list',
-                cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
-              },
-              {
-                id: '2',
-                header: 'Second list',
-                cardIds: ['b', 'c', 'd', 'f', 'h', 'i', 'k'],
-              },
-              {
-                id: '3',
-                header: 'Third list',
-                cardIds: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ],
-              },
-              {
-                id: '4',
-                header: 'Fourth list',
-                cardIds: [ 'l', 'm' ],
-              },
-            ],
-            allCards: {
-              'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
-              'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' },
-              'c': { id: 'c', title: 'Third card', content: 'lorem ipsum' },
-              'd': { id: 'd', title: 'Fourth card', content: 'lorem ipsum' },
-              'e': { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
-              'f': { id: 'f', title: 'Sixth card', content: 'lorem ipsum' },
-              'g': { id: 'g', title: 'Seventh card', content: 'lorem ipsum' },
-              'h': { id: 'h', title: 'Eighth card', content: 'lorem ipsum' },
-              'i': { id: 'i', title: 'Ninth card', content: 'lorem ipsum' },
-              'j': { id: 'j', title: 'Tenth card', content: 'lorem ipsum' },
-              'k': { id: 'k', title: 'Eleventh card', content: 'lorem ipsum' },
-              'l': { id: 'l', title: 'Twelfth card', content: 'lorem ipsum' },
-              'm': { id: 'm', title: 'Thirteenth card', content: 'lorem ipsum' },
-            },
-          }
+    state = data;
+
+    listTitle() {
+      return (
+        this.state.lists.map(list => {
+          const { cardIds, header, listId } = list;
+          const cards = cardIds.map(card => {
+            return this.state.allCards[card];
+          });
+          return (
+            <List
+              cards={cards}
+              handleAdd={this.handleAdd}
+              handleDelete={this.handleDelete}
+              key={listId}
+              title={header}
+            />
+          )
+        })
+      )
     }
 
-    headerOnly() {
-        return (
-            this.state.storeIntermediate.lists.map(list => {
-                const theHeader = list.header
-                const theID = list.id
-                const forCardIds = list.cardIds.map(cardId => {
-                    const begin = this.state.storeIntermediate.allCards[cardId]
-                    return begin
-                })
-                return (
-                    <List key={theID} header={theHeader} cards={forCardIds} deleteClicking={this.deleteClickEventHandler} addingClicking={this.addClickEventHandler} />
-                )
-            })
-        )
+    handleAdd(poss) {
+      console.log('clicked! - added', {poss})
     }
 
-    addClickEventHandler(poss) {
-        console.log('clicked! - added', {poss})
-    }
-
-    deleteClickEventHandler = (pseudo) => {
+    handleDelete = (pseudo) => {
         const firstMatch = this.state.storeIntermediate.lists[0].cardIds.filter(id => id === pseudo)[0]
         const secondMatch = this.state.storeIntermediate.lists[1].cardIds.filter(id => id === pseudo)[0]
         const thirdMatch = this.state.storeIntermediate.lists[2].cardIds.filter(id => id === pseudo)[0]
@@ -113,7 +79,7 @@ class App extends React.Component {
                     <h1>Trelloyes!</h1>
                 </header>
                 <div className="App-list">
-                    {this.headerOnly()}
+                    {this.listTitle()}
                 </div>
             </main>
         )
--- src/components-assignment/List.js
+++ src/components-assignment/List.js
@@ -1,34 +1,39 @@
 import React from 'react';
-import './List.css';
 import Card from './Card.js';
+import './List.css';
 
-function List(props) {
-
-    const intermediate = props.cards.map(card => {
-        const cardContent = card.content
-        const cardTitle = card.title
-        const cardThisID = card.id
-        return (
-            <Card pseudo={cardThisID} key={cardThisID} title={cardTitle} content={cardContent} deleteClicking={props.deleteClicking} />
-        )
-    })
-
+function List({ cards, handleAdd, handleDelete, title }) {
+  const intermediate = cards.map(card => {
+    const {content, id, title } = card;
     return (
-        <section className="List">
-
-
-            <header className="List-header">
-                <h2>{props.header}</h2>
-            </header>
-
-            <div className="List-cards">
-                {intermediate}
-            <button type="button" className="List-add-button" onClick={() => props.addingClicking(props.header)}> + Add Random Card</button>
-            </div>
-
-
-        </section>
+      <Card
+        content={content}
+        handleDelete={handleDelete}
+        id={id}
+        key={id}
+        title={title} 
+      />
     )
+  })
+
+  return (
+    <section className="List">
+
+      <header className="List-header">
+        <h2>{title}</h2>
+      </header>
+
+      <div className="List-cards">
+        {intermediate}
+        <button
+          className="List-add-button"
+          onClick={() => handleAdd(title)}
+          type="button">
+            + Add Random Card
+        </button>
+      </div>
+    </section>
+  )
 }
 
-export default List
+export default List;
